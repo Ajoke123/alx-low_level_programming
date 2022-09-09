bae0d8fa958baed3905ca470da792747606d6972
Niniola <olalabininiola@gmail.com>
2022-09-08 18:15:12 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  *  main - Entry point
+ */
+int main(void)
+{
+		char str[100] = "\"Programming is like building a multilingual puzzle";
+
+			puts(str);
+			return (0);
+}
