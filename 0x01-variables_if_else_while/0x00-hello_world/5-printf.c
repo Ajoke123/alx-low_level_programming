e47e14ba7cc8425914f4b48bb77211901234daac
Niniola <olalabininiola@gmail.com>
2022-09-12 16:59:40 -0700

    task 5

---
+++ 0x01-variables_if_else_while/0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: art
+ *
+ * Return: always (0) (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
