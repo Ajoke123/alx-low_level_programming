4959b5f67306ffb208cc0954e7ad4cd74bb42f22
Niniola <olalabininiola@gmail.com>
2022-09-13 08:44:06 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <main.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ *
+ * description: prints _putchar
+ *
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	putchar('\n');
+	return (0);
+}
