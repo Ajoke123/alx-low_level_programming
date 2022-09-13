bff5e8ee77bbae67c03f84fa1fe81827cc7f5d61
Niniola <olalabininiola@gmail.com>
2022-09-12 17:31:35 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: useful art
+ *
+ * Return: returs 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora kopar, 2015-10-19\n", 59);
+	return (1);
+}
