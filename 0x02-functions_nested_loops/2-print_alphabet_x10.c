6d9d2250b97b97d5a6bd0745ff246f28ccf40951
Niniola <olalabininiola@gmail.com>
2022-09-13 18:00:40 -0700

    task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print alphabet
+ *
+ * Return: (0)
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet, g;
+
+	for (g = 0; g <= 9; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+}
+
