907aed64714f9f4bf83af155e7b4c81d1304ac81
Niniola <olalabininiola@gmail.com>
2022-09-22 06:47:57 -0700

    task 04

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev -> printing a string in reverse
- * @s: the string to be printed in rev
+ * @s: the string to be printed in reverse
  */
 void print_rev(char *s)
 {
@@ -11,6 +11,7 @@ void print_rev(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 
 	for (--i; i >= 0; i--)
+
 		putchar(s[i]);
 		putchar('\n');
 }
