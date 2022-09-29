6443e98f4b9ddab621e8bb88206f6a3fedaba45a
Niniola <olalabininiola@gmail.com>
2022-09-29 16:40:50 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power of x raised to y
+ * @x: 1st int
+ * @y: 2nd int
+ * Return: (_pow_recursion)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y % 2 == 0)
+	{
+		int even = _pow_recursion(x, y / 2);
+
+		return (even * even);
+	}
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
